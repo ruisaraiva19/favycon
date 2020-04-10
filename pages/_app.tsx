e9e3f96bc475e46c8e3f6421e16b9e37efd2fbd5
Rui Saraiva <ruisaraiva19@gmail.com>
2020-04-10 19:07:37 +0100

    ci: add Semantic Pull Requests config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
+import React from 'react'
 import { AppProps } from 'next/app'
 
 import '../styles/main.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />
 }
 
 export default MyApp
