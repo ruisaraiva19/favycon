a99d810f4cf9bc2ed4eb106f7ced741136541a87
Rui Saraiva <ruisaraiva19@gmail.com>
2020-04-12 21:50:14 +0100

    feat(seo): add seo meta tags

--- components/seo/index.tsx
+++ components/seo/index.tsx
@@ -13,6 +13,17 @@ const SEO = ({ title, description }: SEOProps) => (
 		<link rel="icon" type="image/svg+xml" href="/favicon.svg?v=1" />
 		<link rel="icon" type="image/png" href="/favicon.png?v=1" />
 		<link rel="icon" type="image/x-icon" href="/favicon.ico?v=1" />
+		<meta property="og:title" content={title} />
+		<meta property="og:description" content={description} />
+		<meta property="og:type" content="website" />
+		<meta property="og:site_name" content={title} />
+		<meta property="og:url" content="https://favycon.now.sh" />
+		<meta property="og:image" content="https://favycon.now.sh/share.png?v=1" />
+		<meta property="og:image:secure_url" content="https://favycon.now.sh/share.png?v=1" />
+		<meta property="og:image:type" content="image/png" />
+		<meta property="og:image:width" content="1200" />
+		<meta property="og:image:height" content="630" />
+		<meta property="og:image:alt" content={description} />
 	</Head>
 )
 
