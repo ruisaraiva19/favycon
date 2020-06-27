b537d1d56a18657a4b02e98aed3300e1f81cf414
Rui Saraiva <ruisaraiva19@gmail.com>
2020-06-27 21:43:29 +0100

    chore(title): change ios title

--- components/seo/index.tsx
+++ components/seo/index.tsx
@@ -19,6 +19,7 @@ const SEO = ({ title, description }: SEOProps) => (
 		<link rel="apple-touch-icon" sizes="144x144" href="/favicon-144x144.png" />
 		<link rel="apple-touch-icon" sizes="152x152" href="/favicon-152x152.png" />
 		<link rel="apple-touch-icon" sizes="180x180" href="/favicon-180x180.png" />
+		<meta name="apple-mobile-web-app-title" content="Favycon" />
 		<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
