e56b36c158d98baea1dc0ec74a600d0fb5f4f01c
Rui Saraiva <ruisaraiva19@gmail.com>
2021-12-12 22:07:05 +0000

    feat: add security headers

--- next.config.js
+++ next.config.js
@@ -4,4 +4,29 @@ module.exports = {
 		deviceSizes: [375, 750, 1125],
 		imageSizes: [56, 112, 185, 370, 370, 375, 540, 740, 750, 1080],
 	},
+	headers() {
+		return [
+			{
+				source: '/(.*)',
+				headers: [
+					{
+						key: 'X-XSS-Protection',
+						value: '1; mode=block',
+					},
+					{
+						key: 'X-Frame-Options',
+						value: 'SAMEORIGIN',
+					},
+					{
+						key: 'X-Content-Type-Options',
+						value: 'nosniff',
+					},
+					{
+						key: 'Content-Security-Policy',
+						value: `default-src 'self'; frame-ancestors 'self'`,
+					},
+				],
+			},
+		]
+	},
 }
